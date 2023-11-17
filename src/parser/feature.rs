e88e3c3b0fd0ffe31fbbc120b30aac19fad9963b
Carl Gützkow <cjgutzkow@gmail.com>
2023-11-17 23:40:12 +0100

    get coordinates from feature

--- src/parser/feature.rs
+++ src/parser/feature.rs
@@ -4,15 +4,38 @@ use crate::file_rep::lines::DefinitionData;
 
 use super::keys::parse_definition_key;
 
+pub fn str_to_coords(text: &str) -> Result<(f64, f64), ()> {
+    let mut split = text.split(" ");
+    let x = split.next();
+    let y = split.next();
+
+    if x.is_none() || y.is_none() {
+        return Err(());
+    }
+
+    let x = x.unwrap().parse::<f64>();
+    let y = y.unwrap().parse::<f64>();
+
+    if x.is_err() || y.is_err() {
+        return Err(());
+    }
+
+    return Ok((x.unwrap(), y.unwrap()));
+}
+
 pub fn sosi_feature_to_geojson(text: &str) -> Option<Feature> {
     let found = regex::Regex::new(r"\n\d+").unwrap().find(text);
-    println!("{}", found.is_none());
-    dbg!(text);
     if found.is_none() {
         return None;
     }
     let coords_index = found.unwrap().start();
-    let coords = &text[coords_index..];
+    let coords = &text[coords_index..]
+        .trim()
+        .lines()
+        .map(|l| str_to_coords(l))
+        .filter(|c| c.is_ok())
+        .map(|c| c.unwrap())
+        .collect::<Vec<(f64, f64)>>();
 
     let properties = &text[..coords_index]
         .lines()
@@ -20,9 +43,6 @@ pub fn sosi_feature_to_geojson(text: &str) -> Option<Feature> {
         .filter(|d| d.is_some())
         .map(|d| d.unwrap())
         .collect::<Vec<DefinitionData>>();
-
-    dbg!(properties);
-    dbg!(coords);
 
     return None;
 }
